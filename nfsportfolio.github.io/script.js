2df9d894a077229d3271a4657037df8c9ac1a709
wyn <126950082+novflowynsaburnido19@users.noreply.github.com>
2023-04-26 21:31:05 -0700

    Add files via upload

---
+++ nfsportfolio.github.io/script.js
@@ -0,0 +1,28 @@
+// nav link click
+
+// let sections = document.querySelectorAll('section');
+// let navlinks = document.querySelectorAll('navbar a');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height){
+//             navlinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('navbar a[href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
+
+// // sticky
+
+//     let header = document.querySelector('header');
+
+//     header.classList.toggle('sticky', window.scrollY > 100);
+// };
+
+
